324872909727f0ae84c23a37dd784ed61d9ff20c
bourihane <boumilas@yahoo.fr>
2018-02-11 02:29:59 -0500

    titre

--- LMM/client/vues/header.php
+++ LMM/client/vues/header.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 <head>
 <meta charset="UTF-8">
-	<title>Forum de discution</title>	
+	<title>Loue ma maison</title>	
     <meta name="description" content="TP architecture MVC">
   	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
