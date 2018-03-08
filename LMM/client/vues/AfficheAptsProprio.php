2a57a512e7ab09d4d840d66ecc559d60b824d997
Renaud Mercier <rmercier60@gmail.com>
2018-03-08 08:40:29 -0500

    cegep - fin correctifs conflits avec upstream

--- LMM/client/vues/AfficheAptsProprio.php
+++ LMM/client/vues/AfficheAptsProprio.php
@@ -12,13 +12,14 @@
 </div> <!-- fin div row -->
 
 <div class="resultat">
+    
     <div class="row">        
         <?php 
 		if($data["appartements"]){
 			foreach($data["appartements"] as $appartement)
 			{ 
 		?>
-				<div class="col-md-4 appart">
+				<div class="profil-aptProprio d-inline col-sm-4 appart">
 
 					<?php						
 						if ($appartement->getPhotoPrincipale() != "") {
@@ -29,23 +30,31 @@
 												
 					?> 
 					<input type="hidden" value="<?=$appartement->username;?>" id="nomHote"/>  
-					<!--<img src="./images/profil.jpg" alt="mon appart">-->
+					
+                    <!-- Detail d'un appartement -->
 					<div class="thumbnail">
 						<a href="index.php?Appartements&action=afficherAppartement&id_appart=<?=$appartement->getId() ?>" >
 						<img src="<?= $photoApt ?>" class="card-img-top photoAppartement img img-fluid thumbnail" alt="mon appart">
 						</a>
 				 
                         <div class="card-block">			
-                         
-                            <p class="card-text">Hôte: <?=$appartement->username;?></p>                        
+                            
+                            <!--
+                            <p class="card-text">Propriétaire: <?=$appartement->username;?></p>
+                            -->
+                            <h5><?=$appartement->getNoCivique()." ".$appartement->getRue()." ".$appartement->getVille();?> </h5>
+                            <!--
                             <p class="card-text">                            
                                   <small><?= $appartement->getNbPersonnes(); ?>&nbsp<i class="fa fa-male fa-lg"></i>                        
                                     <?= $appartement->getNbChambres(); ?>&nbsp<i class="fa fa-square-o fa-lg"></i>            
                                     <?= $appartement->getNbLits(); ?>&nbsp<i class="fa fa-bed fa-lg"></i> </small>    
                             </p>
-                            <h5 class="card-title"> <?=$appartement->getTitre();?></h5>
+                            
+                            <h5 class="card-title">Titre: <?=$appartement->getTitre();?></h5>
+                            
                             <p class="card-text">$<?=$appartement->getMontantParJour();?> par nuit</p>
-                            <p class="card-text">
+                            -->
+                            <h5 class="card-text">Évalué à 
                                 <?php
                                 for($i=1; $i<=$appartement->moyenne/2; $i++)
                                 {
@@ -70,27 +79,47 @@
                                 }
                                 ?>
                                 <small calss="text-muted"><?=$appartement->NbNotes;?></small>
-                            </p>
-                            <p> <?=$appartement->getNoCivique()." ".$appartement->getRue()." ".$appartement->getVille();?>
-                            </p>
-                    </div>
-                </div>
+                            </h5>
+                            <!--
+                            <p>Logis situé au <?=$appartement->getNoCivique()." ".$appartement->getRue()." ".$appartement->getVille();?> </p>
+                            -->
+                        </div>
+                        
+                
 				<?php 
 					if($appartement->username == $_SESSION['username'])
 					{
 				?>
-						<p class="card-text">
-<!-- @ replacer -->         <p><a class="btn btn-primary" href="index.php?Appartements&action=afficherInscriptionApt&id=<?=$appartement->getId()?>" role="button">Modifier ce logis</a></p> 
-<!-- @ replacer -->         <p><a value="<?=$appartement->getId()?>" class="btn btn-primary btnSuppressionApt" href="#" role="button">Supprimer ce logis</a></p> 
-<!-- @ replacer -->         <p><a class="btn btn-primary" href="index.php?Appartements&action=afficherFormulaireImage&id=<?= $appartement->getId(); ?>" role="button">Ajout de photos</a></p> 
-
-							<form>
-								<input type="hidden" value="<?=$appartement->getId_userProprio()?>">
-								<input classe="idAptSuppression" type="hidden" value="<?=$appartement->getId()?>">
-								<button type="button" data-toggle="modal" data-target="#modal<?=$appartement->getId();?>"  class="btn btn-primary mb-2" >Disponibilite</button>
-							</form>
-						</p>
-						<!-- Modal -->
+                        <br>
+                        
+                        <!-- Options de gestion d'un appartement -->
+                        <div class="profil-aptProprio-opt row justify-content-around">
+				            <div class="d-inline">
+<!-- @ replacer -->             <p><a class="" href="index.php?Appartements&action=afficherInscriptionApt&id=<?=$appartement->getId()?>"><i class="fa fa-pencil-square-o fa-2x"></i></a></p>
+                            </div>
+                            <div class="d-inline">
+<!-- @ replacer -->             <p><a value="<?=$appartement->getId()?>" class="btnSuppressionApt" href="#" ><i class="fa fa-trash-o fa-2x"></i></a></p>
+                            </div>
+                            <div class="d-inline">
+<!-- @ replacer -->             <p><a class="" href="index.php?Appartements&action=afficherFormulaireImage&id=<?= $appartement->getId(); ?>"><i class="fa fa-picture-o fa-2x"></i></a></p>
+                            </div>
+                            <div class="d-inline">
+                                <!--
+                                <form>
+                                    <input type="hidden" value="<?=$appartement->getId_userProprio()?>">
+                                    <input classe="idAptSuppression" type="hidden" value="<?=$appartement->getId()?>">
+                                    <button type="button" data-toggle="modal" data-target="#modal<?=$appartement->getId();?>"  class="" ><i class="fa fa-calendar-check-o fa-2x"></i></button>
+                                </form>
+                                -->
+                                <p><a value="<?=$appartement->getId()?>" class="" data-toggle="modal" data-target="#modal<?=$appartement->getId();?>" href="#" ><i class="fa fa-calendar fa-2x"></i></a></p>
+                            </div>
+                        </div>
+        
+                        <hr>
+                        
+                    </div>
+        
+						<!-- Modal Disponibilite -->
 						<div class="modal fade" id="modal<?=$appartement->getId()?>" tabindex="-1" role="dialog" aria-labelledby="" aria-hidden="true" data-animation="false">
 							<div class="modal-dialog modal-lg" role="document">
 								<div class="modal-content">
@@ -149,6 +178,7 @@
 		}
 		?> 
 	</div>
+
 </div>
 
 
